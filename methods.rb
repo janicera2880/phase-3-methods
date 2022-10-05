c33ba457eac67ecfc72ed09125cd8df0ede5ee27
Janice <janiceralecha@gmail.com>
2022-10-04 21:33:36 -0500

    commit tree c07c04443ac2ce49f60a9b3b0e88ee79afc51046

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+# greet programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end
+# greet Naureen
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+greet
+greet "Jimmy"
+
+# greet with Default
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+greet_with_default "Sunny"
+
+# add
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve (num)
+    if num.class != Integer
+       return nil
+    end
+    num/2
+end
+
+
